67751103643a9936abdb1b8e7f156d7e00565945
djblanc360 <djblanc360@gmail.com>
2018-11-16 14:41:00 -0800

    news add gallery

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ get_header();
         else if ( has_post_format( 'gallery' ) ) {
 
           foreach ( $gallery as $attachment_id => $attachment ) {
-              echo '<gallery style="" controls loop src="' . wp_get_attachment_url( $attachment_id ) . '"></gallery>';
+              echo '<image style="" controls loop src="' . wp_get_attachment_url( $attachment_id ) . '"></image>';
         }
 
         }
