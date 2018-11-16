e8a33e319cca516beeff8839a19f078a27930fcb
djblanc360 <djblanc360@gmail.com>
2018-11-16 15:55:13 -0800

    fix news styling

--- index.php
+++ index.php
@@ -85,12 +85,7 @@ get_header();
 
             <h6 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h6>
 						<P>
-							<?php the_author(); ?>  |	<span class="post-link">
-                <?php
-                  foreach (get_categories() as $category){
-                  echo $category->name;
-                  } ?>
-               </span>
+							<?php the_author(); ?>  |	<span class="post-link"> <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?> </span>
 						</P>
 						<hr>
         <div class="post-text"> <?php the_excerpt(); ?> </div>
