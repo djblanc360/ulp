e035a43900829e3fc59af577179fdbba1c2dd954
djblanc360 <djblanc360@gmail.com>
2018-11-17 20:16:22 -0800

    news post likes

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ get_header();
 				    <div class="clearfix"></div>
 
 				<p>
-				<?php echo get_comments_number(); ?> comments  |	<?php echo get_the_date(); ?> |
+				<?php echo get_comments_number(); ?> comments  |	<?php echo get_the_date(); ?> | <?php echo get_like_count( $like_count ) ?>
 				</p>
     <div class="clearfix"></div>
 
