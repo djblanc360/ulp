c66954375fe5274cbe033231a625029bb9705ce4
djblanc360 <djblanc360@gmail.com>
2018-11-18 17:02:24 -0800

    move like code out of functions

--- inc/post-like.php
+++ inc/post-like.php
@@ -1,3 +1,4 @@
+<?php
 add_action( 'wp_enqueue_scripts', 'sl_enqueue_scripts' );
 function sl_enqueue_scripts() {
 	wp_enqueue_script( 'simple-likes-public-js', get_template_directory_uri() . '/js/simple-likes-public.js', array( 'jquery' ), '0.5', false );
