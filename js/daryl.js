0ab5df478f12b9d0136a29d3463259310f9bdc5f
djblanc360 <djblanc360@gmail.com>
2018-11-19 03:19:18 -0800

    add news slider

--- js/daryl.js
+++ js/daryl.js
@@ -10,26 +10,50 @@ jQuery(window).load(function() {
   /* News SLider For Gallery Posts */
 
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
+  var triggers = $('ul.triggers li');
+  var images = $('ul.news-slider li');
+  var lastElem = triggers.length-1;
+  var target;
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  triggers.first().addClass('active');
+  images.hide().first().show();
+
+  function sliderResponse(target) {
+      images.fadeOut(300).eq(target).fadeIn(300);
+      triggers.removeClass('active').eq(target).addClass('active');
   }
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  triggers.click(function() {
+      if ( !$(this).hasClass('active') ) {
+          target = $(this).index();
+          sliderResponse(target);
+          resetTiming();
+      }
+  });
+
+  $('.next').click(function() {
+      target = $('ul.triggers li.active').index();
+      target === lastElem ? target = 0 : target = target+1;
+      sliderResponse(target);
+      resetTiming();
+  });
+  $('.prev').click(function() {
+      target = $('ul.triggers li.active').index();
+      lastElem = triggers.length-1;
+      target === 0 ? target = lastElem : target = target-1;
+      sliderResponse(target);
+      resetTiming();
+  });
+
+  function sliderTiming() {
+      target = $('ul.triggers li.active').index();
+      target === lastElem ? target = 0 : target = target+1;
+      sliderResponse(target);
   }
 
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("news-single-slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    slides[slideIndex-1].style.display = "block";
+  var timingRun = setInterval(function() { sliderTiming(); },5000);
+  function resetTiming() {
+      clearInterval(timingRun);
+      timingRun = setInterval(function() { sliderTiming(); },5000);
   }
 });
