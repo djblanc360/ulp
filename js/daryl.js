407d8dca4e70cd3d156163270213785ca718df30
djblanc360 <djblanc360@gmail.com>
2018-11-19 01:46:41 -0800

    add news slider

--- js/daryl.js
+++ js/daryl.js
@@ -1,35 +1,34 @@
-/* Masonry */
-
 jQuery(window).load(function() {
 
+  /* Masonry */
   var container = document.querySelector('#ms-container');
   var msnry = new Masonry( container, {
     itemSelector: '.ms-item',
     columnWidth: '.ms-item',
   });
 
-});
+  /* News SLider For Gallery Posts */
+  var slideIndex = 1;
+  showSlides(slideIndex);
 
-
-/* News SLider For Gallery Posts */
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("news-slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
-  slides[slideIndex-1].style.display = "block";
-}
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("news-slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+
+
+});
