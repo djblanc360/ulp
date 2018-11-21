046fc7d013c4c0ba627f952b28e7ae43140b3ade
djblanc360 <djblanc360@gmail.com>
2018-11-20 20:46:19 -0800

    news like count

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ get_header();
 				<a href="<?php the_permalink(); ?>" class="btn btn-green read-more-button">Read More</a>
 				    <div class="clearfix"></div>
 				<p>
-				<?php echo get_comments_number(); ?> comments  |	<?php echo get_the_date(); ?> | <?php echo get_like_count( $count ); ?> <?php echo   get_post_meta( $postID, "likes_number", true ); ?>
+				<?php echo get_comments_number(); ?> comments  |	<?php echo get_the_date(); ?> | Likes  <?php echo   get_post_meta( $postID, "_liked", true ); ?>
 				</p>
     <div class="clearfix"></div>
 
